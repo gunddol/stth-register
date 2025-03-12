cac38f1a7b6b492f618020d2ea472373baff4f6d
gunddol <gunddol1004@naver.com>
2025-03-12 16:42:35 +0900

    스윙투앱 가입절차 코드 완전 삭제

--- selenium_script.py
+++ selenium_script.py
@@ -46,34 +46,6 @@ def cafe24_register(driver, id, pw, name, phone, mail):
     driver.find_element(By.ID, "email1").send_keys(mail)
     time.sleep(0.5)
     driver.find_element(By.CSS_SELECTOR, "div.ec-base-button.gBottom a:nth-child(2)").click()
-    time.sleep(2)
-
-def swing2app_register(driver, id, pw, name):
-    swing_url = "https://www.swing2app.co.kr/view/member_list"
-    driver.get(swing_url)
-    time.sleep(3)
-
-    driver.find_element(By.XPATH, "//div[@target='#subAdminLogin']").click()
-    time.sleep(1)
-
-    driver.find_element(By.ID, "appIdSingInIdInput").send_keys('bookboostth')
-    time.sleep(0.5)
-    driver.find_element(By.ID, "subAdminSingInIdInput").send_keys('admin3')
-    time.sleep(0.5)
-    driver.find_element(By.ID, "subAdminSingInPassWordInput").send_keys('1234')
-    time.sleep(0.5)
-    driver.find_element(By.XPATH, "//button[@class='fxt-btn-fill sub-admin-login-btn']").click()
-    time.sleep(8)
-
-    driver.find_element(By.XPATH, "//div[@class='btn btn-default app-user-create-popup-open-btn']").click()
-    time.sleep(3)
-    driver.find_element(By.XPATH, "//input[@class='form-control user-id']").send_keys(id)
-    time.sleep(0.5)
-    driver.find_element(By.XPATH, "//input[@class='form-control user-pw']").send_keys(pw)
-    time.sleep(0.5)
-    driver.find_element(By.XPATH, "//input[@class='form-control user-name']").send_keys(name)
-    time.sleep(0.5)
-    driver.find_element(By.XPATH, "//button[@class='btn btn-success app-user-save-btn']").click()
     time.sleep(3)
 
 def main():
@@ -94,14 +66,8 @@ def main():
         test_mail = args.mail
 
         cafe24_register(driver, test_id, test_pw, test_name, test_phone, test_mail)
-        # swing2app_register(driver, test_id, test_pw, test_name)
-
-        with open("output.txt", "w") as f:
-            f.write("✅ Registration successful!")
-
     except Exception as e:
-        with open("output.txt", "w") as f:
-            f.write(f"❌ Error: {str(e)}")
+        print(f"❌ Error: {str(e)}")
 
     finally:
         driver.quit()
