bdb673da80da8a5b8420ee5bc0a6f04c16fbee2b
gunddol <gunddol1004@naver.com>
2025-03-12 16:57:01 +0900

    모두 동의 체크박스 오류 수정 #1

--- selenium_script.py
+++ selenium_script.py
@@ -21,9 +21,9 @@ def create_driver():
 def cafe24_register(driver, id, pw, name, phone, mail):
     target_url = "https://suraktantan.cafe24.com/member/agreement.html"
     driver.get(target_url)
-    time.sleep(8)
+    time.sleep(5)
 
-    driver.find_element(By.CSS_SELECTOR, ".agreeAll .ec-base-chk input").click()
+    driver.find_element(By.ID, "sAgreeAllChecked").click()
     time.sleep(1)
     driver.find_element(By.CSS_SELECTOR, "div.ec-base-button.gBottom button").click()
     time.sleep(1)
