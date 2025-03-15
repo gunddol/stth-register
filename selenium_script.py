8d509eee42935b023c7ba1866dd2a78d79cc516c
gunddol <gunddol1004@hanyang.ac.kr>
2025-03-15 20:02:26 +0900

    phone number fixed

--- selenium_script.py
+++ selenium_script.py
@@ -6,72 +6,85 @@ import time
 import argparse
 
 def create_driver():
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
+  chrome_options = Options()
+  chrome_options.add_argument("--headless")
+  chrome_options.add_argument("--disable-gpu")
+  chrome_options.add_argument("--no-sandbox")
+  chrome_options.add_argument("--disable-dev-shm-usage")
 
-    service = Service("/usr/bin/chromedriver")
-    driver = webdriver.Chrome(service=service, options=chrome_options)
-    
-    return driver
+  service = Service("/usr/bin/chromedriver")
+  driver = webdriver.Chrome(service=service, options=chrome_options)
+  
+  return driver
 
 
 def cafe24_register(driver, id, pw, name, phone, mail):
-    target_url = "https://suraktantan.cafe24.com/member/agreement.html"
-    driver.get(target_url)
-    time.sleep(10)
+  target_url = "https://suraktantan.cafe24.com/member/agreement.html"
+  driver.get(target_url)
+  time.sleep(5)
 
-    check_element = driver.find_element(By.ID, "sAgreeAllChecked")
-    driver.execute_script("arguments[0].click();", check_element)
-    time.sleep(1)
-    driver.find_element(By.CSS_SELECTOR, "div.ec-base-button.gBottom button").click()
-    time.sleep(1)
+  check_element = driver.find_element(By.ID, "sAgreeAllChecked")
+  driver.execute_script("arguments[0].click();", check_element)
+  time.sleep(1)
+  driver.find_element(By.CSS_SELECTOR, "div.ec-base-button.gBottom button").click()
+  time.sleep(1)
 
-    driver.find_element(By.ID, "member_id").send_keys(id)
-    time.sleep(0.5)
-    driver.find_element(By.ID, "passwd").send_keys(pw)
-    time.sleep(0.5)
-    driver.find_element(By.ID, "user_passwd_confirm").send_keys(pw)
-    time.sleep(0.5)
-    driver.find_element(By.ID, "name").send_keys(name)
-    time.sleep(0.5)
-    
+  driver.find_element(By.ID, "member_id").send_keys(id)
+  time.sleep(0.5)
+  driver.find_element(By.ID, "passwd").send_keys(pw)
+  time.sleep(0.5)
+  driver.find_element(By.ID, "user_passwd_confirm").send_keys(pw)
+  time.sleep(0.5)
+  driver.find_element(By.ID, "name").send_keys(name)
+  time.sleep(0.5)
+  
+  if '-' in phone:
     phone_parts = phone.split('-')
     driver.find_element(By.ID, "mobile2").send_keys(phone_parts[1])
     time.sleep(0.5)
     driver.find_element(By.ID, "mobile3").send_keys(phone_parts[2])
     time.sleep(0.5)
-
-    driver.find_element(By.ID, "email1").send_keys(mail)
+  else:
+    phone_number = phone.replace('010', '')
+    middle_digits = phone_number[:4]
+    last_digits = phone_number[4:]
+    
+    driver.find_element(By.ID, "mobile2").send_keys(middle_digits)
     time.sleep(0.5)
-    driver.find_element(By.CSS_SELECTOR, "div.ec-base-button.gBottom a:nth-child(2)").click()
-    time.sleep(3)
+    driver.find_element(By.ID, "mobile3").send_keys(last_digits)
+    time.sleep(0.5)
+
+  driver.find_element(By.ID, "email1").send_keys(mail)
+  time.sleep(0.5)
+  driver.find_element(By.CSS_SELECTOR, "div.ec-base-button.gBottom a:nth-child(2)").click()
+  time.sleep(2)
+
 
 def main():
-    parser = argparse.ArgumentParser(description="Run Selenium script with arguments")
-    parser.add_argument("--id", required=True, help="ID")
-    parser.add_argument("--pw", required=True, help="Password")
-    parser.add_argument("--name", required=True, help="Name")
-    parser.add_argument("--phone", required=True, help="PhoneNumber")
-    parser.add_argument("--mail", required=True, help="Email")
-    args = parser.parse_args()
+  parser = argparse.ArgumentParser(description="Run Selenium script with arguments")
+  parser.add_argument("--id", required=True, help="ID")
+  parser.add_argument("--pw", required=True, help="Password")
+  parser.add_argument("--name", required=True, help="Name")
+  parser.add_argument("--phone", required=True, help="PhoneNumber")
+  parser.add_argument("--mail", required=True, help="Email")
+  args = parser.parse_args()
 
-    driver = create_driver()
-    try:
-        test_id = args.id
-        test_pw = args.pw
-        test_name = args.name
-        test_phone = args.phone
-        test_mail = args.mail
+  driver = create_driver()
+  try:
+    test_id = args.id
+    test_pw = args.pw
+    test_name = args.name
+    test_phone = args.phone
+    test_mail = args.mail
 
-        cafe24_register(driver, test_id, test_pw, test_name, test_phone, test_mail)
-    except Exception as e:
-        print(f"❌ Error: {str(e)}")
+    cafe24_register(driver, test_id, test_pw, test_name, test_phone, test_mail)
 
-    finally:
-        driver.quit()
+
+  except Exception as e:
+    print(f"❌ Error: {str(e)}")
+
+  finally:
+    driver.quit()
 
 if __name__ == "__main__":
-    main()
+  main()
